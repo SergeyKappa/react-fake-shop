2e78a21ed31b233f15ce4307c1c21beebd11d815
Sergii <kapanasik1901@gmail.com>
2024-07-26 14:25:36 +0700

    add Header and Text components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,79 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello, React.js</h1>
-ReactDOM.render(element, document.getElementById('root'))
+// const element = (
+//     <>
+//         <h1>Hello, React.js</h1>
+//         <p>
+//             Додатки, створені за допомогою самого лише React, зазвичай мають
+//             лише один кореневий вузол DOM. Якщо ви інтегруєте React в існуючий
+//             додаток — ви можете мати будь-яку кількість ізольованих кореневих
+//             DOM вузлів.
+//         </p>
+//         <p>
+//             Для рендерингу React-елементу в кореневому DOM вузлі, викличте
+//             функцію ReactDOM.render() з React-елементом та кореневим DOM вузлом
+//             у якості аргументів:
+//         </p>
+//     </>
+// )
+
+// function App() {
+//     return (
+//         <>
+//             <h1>Hello, i'm App Component.</h1>
+//             <p>
+//                 Додатки, створені за допомогою самого лише React, зазвичай мають
+//                 лише один кореневий вузол DOM. Якщо ви інтегруєте React в
+//                 існуючий додаток — ви можете мати будь-яку кількість ізольованих
+//                 кореневих DOM вузлів.
+//             </p>
+//         </>
+//     )
+// }
+const Header = () => {
+    return <h1>Hello everyone: i start learn React!!! </h1>
+}
+
+const Text = () => {
+    return (
+        <>
+            <p>
+                Фундаментально, JSX является синтаксическим сахаром для функции
+                React.createElement(component, props, ...children).
+            </p>
+            <p>
+                Т.к. JSX компилируется в вызовы функции React.createElement,
+                библиотека React всегда должна находиться в области видимости
+                вашего кода JSX.
+            </p>
+        </>
+    )
+}
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Text />
+        </>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+//ReactDOM.render(element, document.getElementById('root'))
 //const root = ReactDOM.createRoot(document.getElementById('root'))
+//root.render(<App/>);
 //root.render(element);
 
-
 //
-function helle(){
-    const name = 'Иван-Царевич';
-    const ele = <h1>Здравствуй, {name}!</h1>;
+function helle() {
+    const name = 'Иван-Царевич'
+    const ele = <h1>Здравствуй, {name}!</h1>
     //root.render(ele)
 }
 // setInterval(helle, 1500)
-
 
 //
 function tick() {
@@ -26,54 +85,43 @@ function tick() {
         </div>
     )
     //root.render(ent)
-  
 }
 // setInterval(tick, 2000)
 
-
-
 //
 function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-  }
-  
-  const user = {
+    return user.firstName + ' ' + user.lastName
+}
+
+const user = {
     firstName: 'Алесандер',
-    lastName: 'Хацкевич'
-  };
-  
-  const elem = (
-    <h1>
-      Здравствуй, {formatName(user)}!
-    </h1>
-  );
-  //root.render(elem)
+    lastName: 'Хацкевич',
+}
 
-
-  //
-  function getGreeting(user) {
-    if (user) {
-      return <h1>Здравствуй, {formatName(user)}!</h1>;
-    }
-    return <h1>Здравствуй, незнакомец.</h1>;
-  }
-  //root.render(getGreeting())
-
-
+const elem = <h1>Здравствуй, {formatName(user)}!</h1>
+//root.render(elem)
 
 //
-  const eleent = (
+function getGreeting(user) {
+    if (user) {
+        return <h1>Здравствуй, {formatName(user)}!</h1>
+    }
+    return <h1>Здравствуй, незнакомец.</h1>
+}
+//root.render(getGreeting())
+
+//
+const eleent = (
     <div>
-      <h1>Здравствуйте!</h1>
-      <h2>Рады вас видеть.</h2>
+        <h1>Здравствуйте!</h1>
+        <h2>Рады вас видеть.</h2>
     </div>
-  );
-  //root.render(eleent)
+)
+//root.render(eleent)
 
-
-  //
-  function Welcome(props) {
-    return <h1>Привет, {props.name}</h1>;
-  }
-  const ement = <Welcome name="Алиса" />;
-  //root.render(ement);// Привет Алиса
+//
+function Welcome(props) {
+    return <h1>Привет, {props.name}</h1>
+}
+const ement = <Welcome name="Алиса" />
+//root.render(ement);// Привет Алиса
