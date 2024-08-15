dfa636f6677dde55373547ddbf22bcba70486a1a
Sergii <kapanasik1901@gmail.com>
2024-08-15 18:38:02 +0700

    add productslist

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,26 +1,27 @@
-import { Typography } from '@mui/material'
 import React from 'react'
+import { Typography } from '@mui/material'
+import './ProductsList.css'
 
-const ProductsList = () => {
-    let uppercase = 'uppercase'
-    let lowercase = 'lowercase'
-    let state = false
+export const ProductsList = () => {
+    // let uppercase = 'uppercase'
+    // let lowercase = 'lowercase'
+    // let state = false
+
     return (
         <>
             <Typography
                 align="center"
                 component="h1"
                 variant="h4"
-                style={{
-                    textTransform: state ? uppercase : lowercase,
-                    color: 'blue',
-                    margin: '30px 0',
-                }}
+                className="title"
+                // style={{
+                //     textTransform: state ? uppercase : lowercase,
+                //     color: 'blue',
+                //     margin: '30px 0',
+                // }}
             >
                 Products List
             </Typography>
         </>
     )
 }
-
-export default ProductsList
--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { Container } from '@mui/material'
-import ProductsList from 'components/Products/ProductsList'
-
+import { ProductsList } from 'components/Products/ProductsList'
 
 export const Main = () => {
-    
     return (
         <Container>
-          <ProductsList/>
+            <ProductsList />
         </Container>
     )
 }
-
-
